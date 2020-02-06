f6982be59152742af27247323e5a644592ed66c5
FredrikPedersen <fredrikhp@gmail.com>
2020-02-06 23:19:00 +0100

    Created Init method for filling movie selector and adding some initial ticket data.

--- ObligatoryAssignments/Oblig1/js/script.js
+++ ObligatoryAssignments/Oblig1/js/script.js
@@ -1,3 +1,47 @@
+let tickets = [];
+const movies = ["Avengers: Secret Wars", "Black Widow", "1917", "Life on the Datatorg: A Documentary"];
+
+//Method to be called when the DOM is rendered.
+const init = () => {
+    createSampleTickets();
+    fillMovieSelector();
+};
+const createSampleTickets = () => {
+    if (localStorage.getItem("ticketsArray") === null) {
+        tickets.push(
+            {
+                movie: "Avengers: Secret Wars",
+                quantity: 5,
+                firstName: "Fredrik",
+                lastName: "Pedersen",
+                phoneNumber: 87654321,
+                email: "fredrik@mail.com"
+            },
+            {
+                movie: "Black Widow",
+                quantity: 2,
+                firstName: "Signe",
+                lastName: "Eide",
+                phoneNumber: 12345678,
+                email: "signe@mail.com"
+            }
+        );
+    } else {
+        tickets = JSON.parse(localStorage.getItem("ticketsArray"));
+    }
+    console.log(tickets);
+};
+
+const fillMovieSelector = () => {
+    const movieSelector = document.getElementById("movieSelector");
+
+    movies.forEach(movie => {
+        const option = document.createElement("option");
+        option.value = movie;
+        option.innerHTML = movie;
+        movieSelector.appendChild(option);
+    });
+};
 //Code mostly taken from Bootstrap's documentation on how to do form validation.
 ( () => {
     window.addEventListener('load',  () => {
